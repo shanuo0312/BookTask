8ee5f8de0ea3df7103e4739372b91d8f4d6fb117
unknown <xingyangyang@bj-xingyy.wenxue.root>
2017-02-20 18:57:54 +0800

    task schedule

--- src/com/qq/book/task/App.java
+++ src/com/qq/book/task/App.java
@@ -12,7 +12,7 @@ public class App {
 	public static void main(String [] args){
 		SpringUtils.init();
 		try {
-			SpringUtils.getExecutor().addJob("test-6", DemoJob.class, "0 30 18 * * ?");     //秒、分、时、日、月、周
+			SpringUtils.getExecutor().addJob("test-19", DemoJob.class, "10 0 16 * * ?");     //秒、分、时、日、月、周
 		} catch (ParseException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
